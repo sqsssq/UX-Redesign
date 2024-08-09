46386924a171a6dbffc5000e3a4c3060f3ecc838
Qing Shi <44493311+sqsssq@users.noreply.github.com>
2024-08-09 12:23:26 +0800

    Update

--- System/Frontend/src/components/Main.vue
+++ System/Frontend/src/components/Main.vue
@@ -106,6 +106,14 @@ export default {
         startStudy() {
             this.startTag = true;
             const vm = this;
+            // webgazer.setGazeListener(function(data, elapsedTime) {
+            //     if (data == null) {
+            //         return;
+            //     }
+            //     var xprediction = data.x; //these x coordinates are relative to the viewport
+            //     var yprediction = data.y; //these y coordinates are relative to the viewport
+            //     console.log(elapsedTime, xprediction, yprediction); //elapsed time is based on time since begin was called
+            // }).begin();
             this.intervalID = setInterval(() => {
                 vm.saveData();
             }, 600000);
