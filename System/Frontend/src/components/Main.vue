88cec24d401190076ba2606c93d00dbcea175ae2
Qing Shi <44493311+sqsssq@users.noreply.github.com>
2024-08-09 11:19:17 +0800

    Update Main.vue

--- System/Frontend/src/components/Main.vue
+++ System/Frontend/src/components/Main.vue
@@ -88,7 +88,7 @@ export default {
         },
         sendData(data) {
             // https://formspree.io/f/mnnavdze
-            axios.post('https://formspree.io/f/xrgnoavv', {
+            axios.post('https://formspree.io/f/mnnavdze', {
                 data: JSON.stringify(data),
                 dataType: 'json'
             }).then((res) => {
